69a08eaf8d1b4f2d441d30ff064a77b5fc0b3977
Yueming <yl3436@cornell.edu>
2022-09-13 23:06:09 -0400

    MST, SD

--- src/components/CAD/w_MST.js
+++ src/components/CAD/w_MST.js
@@ -1,12 +1,107 @@
 import React from 'react';
 import { ProjectConstainer } from './CADelements';
+
+import styled from "styled-components";
+
+const Details = styled.div`
+margin-top: 20px;
+display: grid;
+grid-template-columns: auto auto auto;
+grid-template-rows: 120px 24px 230px 210px;
+font-size: 20px;
+font-weight: 100;
+font-family: sans-serif;
+grid-gap: 40px;
+`
+
+const Text_1 = styled.div`
+grid-column: 1 / 4;
+grid-row: 1 / 2;
+`
+const Text_2 = styled.div`
+font-size: 24px;
+grid-column: 1 / 4;
+grid-row: 2 / 3;
+`
+const Text_3 = styled.div`
+grid-column: 1 / 2;
+grid-row: 4;
+`
+const Img_1 = styled.img`
+height: 230px;
+grid-column: 1;
+grid-row: 3 /4;
+`
+const Img_2 = styled.img`
+height: 230px;
+grid-column: 2;
+grid-row: 3 /4;
+`
+const Img_3 = styled.img`
+height: 230px;
+grid-column: 3;
+grid-row: 3 /4;
+`
+const Img_4 = styled.img`
+height: 210px;
+grid-column: 2/4;
+grid-row: 4;
+`
+const Img_5 = styled.div`
+margin: 20px 0px;
+`
+const Img_6 = styled.img`
+width: 300px;
+grid-column: 1;
+grid-row: 2 /4;
+`
+
 const MST = () => {
 
   return (
     <>
-      <div id='MST' style={{ background: "#1E1D20", height: "1200px" }}>
+      <div id='MST' style={{ background: "#1E1D20", height: "1250px" }}>
         <ProjectConstainer>
           Music Sheet Turner
+          <Details>
+            <Text_1>
+              This is an open design project for the Mechanical Synthesis class.
+              We are challenged to design and build a purely mechanical system to
+              help improve a small aspect of people's daily life.
+              <p></p>
+              My partner and I interviewed a pianist. We learned that it's inconvenient
+              for her to turn the music sheet pages when both of her hands are engaged
+              in playing. We designed this music sheet turner so that she can use her
+              idle left foot to turn the pages while playing the piano.
+            </Text_1>
+            <Text_2>
+              CAD model vs. Prototype
+            </Text_2>
+
+            <Img_1 src='/images/CAD/MST_1.jpg' />
+            <Img_2 src='/images/CAD/MST_3.jpg' />
+            <Img_3 src='/images/CAD/MST_2.jpg' />
+
+
+            <Text_3>
+              The parts of the turner are connected by a bead chain. To drive the Music Sheet Turner,
+              the users press their feet on the pedal, which triggers the linear motion of the bead chain,
+              which then rotate the scroll to present the next page.
+            </Text_3>
+
+            <Img_4 src='/images/CAD/MST_4.jpg' />
+
+
+          </Details>
+          <Img_5>
+            <iframe width="550" height="350"
+              src="https://www.youtube.com/embed/qBVfzrCr0Uk"
+              title="YouTube video player"
+              frameborder="0"
+              allowfullscreen
+            ></iframe>
+          </Img_5>
+
         </ProjectConstainer>
       </div>
     </>
--- src/components/CAD/z_SD.js
+++ src/components/CAD/z_SD.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import { ProjectConstainer } from './CADelements';
+
+import styled from "styled-components";
+
+const Details = styled.div`
+margin-top: 20px;
+display: grid;
+grid-template-columns: 300px 450px 300px;
+grid-template-rows: 70px 335px 230px;
+font-size: 20px;
+font-weight: 100;
+font-family: sans-serif;
+grid-gap: 40px;
+`
+
+const Text_1 = styled.div`
+grid-column: 1 / 4;
+grid-row: 1 / 2;
+`
+const Img_1 = styled.img`
+width: 300px;
+grid-column: 1;
+grid-row: 2 /4;
+`
+const Img_2 = styled.img`
+width: 450px;
+grid-column: 2;
+grid-row: 2 /3;
+`
+const Img_3 = styled.img`
+width: 450px;
+grid-column: 2;
+grid-row: 3 /4;
+`
+const Img_4 = styled.img`
+width: 300px;
+grid-column: 3/4;
+grid-row: 2 /4;
+`
+
 const SD = () => {
 
   return (
     <>
-      <div id='SD' style={{ background: "#131211", height: "1200px" }}>
+      <div id='SD' style={{ background: "#131211", height: "1000px" }}>
         <ProjectConstainer>
           Sword Design
+          <Details>
+            <Text_1 className='text_1'>
+              I was extremely fanscinated by this decorative sword design by Sarah Morris.
+              <p></p>
+              I CADed and printed the frame of the sword, reinforced it with steel core,
+              and laser cutted acrylic pieces to mimic the look of stained glass.
+            </Text_1>
+            <Img_1 src='/images/CAD/sword_1.jpg' />
+            <Img_2 src='/images/CAD/sword_2.jpg' />
+            <Img_3 src='/images/CAD/sword_3.jpg' />
+            <Img_4 src='/images/CAD/sword_4.jpg' />
+
+          </Details>
         </ProjectConstainer>
       </div>
     </>
